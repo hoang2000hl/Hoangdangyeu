9be019ebbf8c4533ccf624c5b8929467d3aa2e78
hoang2000hl <dvhhlqn18122000@gmail.com>
2022-11-02 14:25:39 +0700

    hoangdangyeu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
+import { TodoListService } from './services/todo-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit {
+  titleApp: String = "Hello";
+  title: any;
+  todoList: TodoItem[];
+  item: TodoItem;
+
+  constructor(private todoListService: TodoListService) { }
+
+  ngOnInit(): void {
+    this.todoList = this.todoListService.getTodoList();
+    console.log(this.todoList);
+    
+  }
+  completeItem(): void {
+  }
+  addItem(title: string): void {
+    this.todoListService.addItem({ title, completed: false });
+    console.log("addItem",this.todoList);
+  }
+  removeItem(item): void {
+    this.todoListService.deleteItem(item);
+  }
+
+
 }
---
+++ src/app/services/todo-list.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+import { StorageService } from './storage.service';
+
+const todoListStorageKey = 'Todo_List';
+const defaultTodoList: TodoItem[] = [
+  { title: 'install NodeJS', completed: false },
+  { title: 'install Angular CLI', completed: false },
+  { title: 'create new app', completed: false },
+  { title: 'serve app', completed: false },
+  { title: 'develop app', completed: false },
+  { title: 'deploy app', completed: false },
+];
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoListService {
+  todoList: TodoItem[];
+  constructor(private storageService: StorageService) { 
+    this.todoList = storageService.getData(todoListStorageKey) || 
+    defaultTodoList;
+  }
+  
+  saveList(): void {
+    this.storageService.setData(todoListStorageKey, this.todoList);
+  }
+
+  getTodoList(): TodoItem[] {
+    return this.todoList;
+   }
+   addItem(item: TodoItem): void {
+    this.todoList.push(item);
+    this.saveList()
+   } 
+   updateItem(item: TodoItem, changes): void {
+    const index = this.todoList.indexOf(item);
+    this.todoList[index] = {...item, ...changes};
+    this.saveList();
+   }
+
+   deleteItem(item: TodoItem): void {
+    const index = this.todoList.indexOf(item);
+    this.todoList.splice(index, 1)
+    this.saveList();
+   }
+}
